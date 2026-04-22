93da1d7f4619ab042a591b1dbb221d2c65ec89c6
rafterminador <ferrafterminador@gmail.com>
2026-04-21 21:24:27 -0600

    feat: adding base folders of the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@css": "/src/css",
+      "@pages": "/src/pages",
+      "@redux": "/src/redux",
+      "@utils": "/src/utils",
+      "@views": "/src/views",
+      "@constants": "/src/constants",
+      "@services": "/src/services",
+      "@routes": "/src/routes",
+    }
+  }
 })
